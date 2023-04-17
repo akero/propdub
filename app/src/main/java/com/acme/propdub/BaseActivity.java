e2f9fc5962c71c218e51d9fbeb5e125c1d4da6ea
pifon <rish1994@gmail.com>
2023-04-17 12:13:57 +0530

    Final skeleton (version 1)

---
+++ app/src/main/java/com/acme/propdub/BaseActivity.java
@@ -0,0 +1,34 @@
+package com.acme.propdub;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.acme.propdub.R;
+import com.google.android.material.navigation.NavigationView;
+
+//TODO: Finish this to add the sidebar icon to all other activities. This base class will get extended from by every activity.
+public class BaseActivity extends AppCompatActivity {
+    protected DrawerLayout drawerLayout;
+    protected NavigationView navigationView;
+    protected Toolbar toolbar;
+
+    protected void setupNavigationDrawer() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.id_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+}
+
+
+
+
+
+
