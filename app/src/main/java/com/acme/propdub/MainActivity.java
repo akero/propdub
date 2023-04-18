b59ae44148e87e930134eeeffaac5938de768143
pifon <rish1994@gmail.com>
2023-04-18 13:25:46 +0530

    finished mortgage calc

--- app/src/main/java/com/acme/propdub/MainActivity.java
+++ app/src/main/java/com/acme/propdub/MainActivity.java
@@ -46,10 +46,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        //appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
