f7547a0419fc31d6ff6fda4c4f6362ac5e4de925
Akero <rish1994@gmail.com>
2023-05-05 10:39:39 +0530

    a

--- app/src/main/java/com/acme/propdub/SplashActivity.java
+++ app/src/main/java/com/acme/propdub/SplashActivity.java
@@ -11,7 +11,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        //test
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
