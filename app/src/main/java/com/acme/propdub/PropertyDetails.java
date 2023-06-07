004f35083744766fe11207cfd63b99ae2c6ce5e6
Akero <rish1994@gmail.com>
2023-06-07 12:09:43 +0530

    lol

--- app/src/main/java/com/acme/propdub/PropertyDetails.java
+++ app/src/main/java/com/acme/propdub/PropertyDetails.java
@@ -28,7 +28,6 @@ import jp.wasabeef.blurry.Blurry;
 
 public class PropertyDetails extends AppCompatActivity {
 
-
     private ImageView menuIcon;
     private NavigationView navigationView;
     MapView mapView;
@@ -43,17 +42,7 @@ public class PropertyDetails extends AppCompatActivity {
             mapView = findViewById(R.id.map_view);
             mapView.onCreate(savedInstanceState);
 
-
             ConstraintLayout rootLayout = findViewById(R.id.root_layout);
-            //View blueTint = findViewById(R.id.blue_tint);
-
-            //Blurry.with(this).radius(10).sampling(8).color(Color.argb(66, 0, 66, 116)).async().onto(rootLayout);
-
-
-            /*ImageView imageView = findViewById(R.id.background_image3);
-            Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.bg_about_us);
-            Bitmap blurredBitmap = BlurBuilder.blur(this, originalBitmap);
-            imageView.setImageBitmap(blurredBitmap); */
 
             ViewPager2 viewPager = findViewById(R.id.viewPager1);
             ImageAdapter1 adapter = new ImageAdapter1(this, imageIds);
@@ -65,79 +54,17 @@ public class PropertyDetails extends AppCompatActivity {
             webSettings.setJavaScriptEnabled(true);
 
             //feed this the correct video id
+
+            String youtube_key=getString(R.string.youtube_key);
             String videoId = "YOUR_YOUTUBE_VIDEO_ID";
-            videoId = "https://www.youtube.com/watch?v=9bZkp7q19f0";
-            String html = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/" + videoId + "\" frameborder=\"0\" allowfullscreen></iframe>";
+            videoId = "9bZkp7q19f0";
+            String html = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/" + videoId + "\" frameborder=\"0\" allowfullscreen></iframe>"+ "&key=" + youtube_key;
             webView.loadData(html, "text/html", "utf-8");
 
-          /*
-            //back button code
-            View backButton = findViewById(R.id.back_button);
-            backButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    onBackPressed();
-                }
-            });
-
-            // contact button code
-            View contactButton = findViewById(R.id.contact);
-            contactButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    openContactActivity();
-                }
-            });
-
-            // mortgage calculator button code
-            View mortgage_calculatorButton = findViewById(R.id.mortgage_calculator);
-            mortgage_calculatorButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    openMortgageActivity();
-                }
-            });
-
-            // Location on map button code
-            View locationOnMapButton = findViewById(R.id.location_on_map);
-            locationOnMapButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    //openPropertySaleActivity();
-                }
-            });
-
-            // schedule button code
-            View scheduleButton = findViewById(R.id.schedule_viewing);
-            scheduleButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    openScheduleButtonActivity();
-                }
-            });
-
-            // video button code
-            View videoButton = findViewById(R.id.video);
-            videoButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    //openPropertySaleActivity();
-                }
-            });
-
-            // documents viewer button code
-            View documentsViewerButton = findViewById(R.id.documents_viewer);
-            documentsViewerButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    openDocViewerActivity();
-                }
-            }); */
         } catch (Exception e) {
             Log.d("tag4", e.toString());
         }
     }
-
 
     // Method to navigate to DocViewer
     private void openDocViewerActivity() {
@@ -163,10 +90,8 @@ public class PropertyDetails extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-
         super.onBackPressed();
     }
-
 
     @Override
     protected void onResume() {
