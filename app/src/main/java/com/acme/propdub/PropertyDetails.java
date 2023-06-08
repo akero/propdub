bb79b7dfffd5dd38d3efa81f00934af0e64fba29
Akero <rish1994@gmail.com>
2023-06-08 09:37:35 +0530

    first commit june 8

--- app/src/main/java/com/acme/propdub/PropertyDetails.java
+++ app/src/main/java/com/acme/propdub/PropertyDetails.java
@@ -90,6 +90,7 @@ public class PropertyDetails extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
+
         super.onBackPressed();
     }
 
--- app/src/main/java/com/acme/propdub/SearchByMap.java
+++ app/src/main/java/com/acme/propdub/SearchByMap.java
@@ -3,12 +3,53 @@ package com.acme.propdub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.MapView;
+
 
 public class SearchByMap extends AppCompatActivity {
 
+    MapView mapView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_by_map);
+
+        try {
+            mapView=findViewById(R.id.map_view);
+            mapView.onCreate(savedInstanceState);
+        }catch(Exception e){
+            Log.d("tag9", e.toString());
+        }
+    }
+
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onDestroy(){
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onPause(){
+        mapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onLowMemory(){
+        mapView.onLowMemory();
+        super.onLowMemory();
     }
 }
