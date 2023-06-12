2404395c304539f8f23747dbbde4b716d84f02dc
Akero <rish1994@gmail.com>
2023-06-12 15:56:50 +0530

    leave a review class done

--- app/src/main/java/com/acme/propdub/ReviewPopup.java
+++ app/src/main/java/com/acme/propdub/ReviewPopup.java
@@ -2,8 +2,15 @@ package com.acme.propdub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class ReviewPopup extends AppCompatActivity {
 
@@ -11,6 +18,43 @@ public class ReviewPopup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review_popup);
+
+        ImageView close;
+        close= findViewById(R.id.close);
+        close.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                onBackPressed();
+            }
+        });
+
+        Button submit, maybelater;
+        submit= findViewById(R.id.button);
+        maybelater= findViewById(R.id.button1);
+
+        EditText message= findViewById(R.id.totalAmountEditText4);
+        submit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                //TODO
+                String message1 = message.getText().toString();
+
+                if (TextUtils.isEmpty(message1)) {
+                    Toast.makeText(ReviewPopup.this, "Please enter your message", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(ReviewPopup.this, ThankYou.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        maybelater.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                onBackPressed();
+            }
+        });
     }
 
     @Override
