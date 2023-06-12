e6776f60fff4ab6334e151b82521b4221dbabd76
Akero <rish1994@gmail.com>
2023-06-12 13:45:55 +0530

    added code to contact us

--- app/src/main/java/com/acme/propdub/Support.java
+++ app/src/main/java/com/acme/propdub/Support.java
@@ -3,11 +3,18 @@ package com.acme.propdub;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.gms.maps.MapView;
@@ -50,6 +57,55 @@ try{
 
 // Set the adapter for the Spinner
         countrySpinner.setAdapter(adapter);
+
+        EditText name, phone, email, message;
+        name = findViewById(R.id.totalAmountEditText);
+        phone = findViewById(R.id.totalAmountEditText1);
+        email = findViewById(R.id.totalAmountEditText2);
+        message = findViewById(R.id.totalAmountEditText4);
+
+        Button submit;
+        submit= findViewById(R.id.button);
+
+        submit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                String name1= name.getText().toString();
+                String phone1= phone.getText().toString();
+                String email1= email.getText().toString();
+                String message1= message.getText().toString();
+
+                if(TextUtils.isEmpty(email1)){
+                    Toast.makeText(Support.this, "Please enter your email", Toast.LENGTH_SHORT).show();
+                }
+                // Check if the input is a valid email
+                else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email1).matches()) {
+                    // Input is not a valid email, show a message to the user
+                    Toast.makeText(Support.this, "Please enter a valid email", Toast.LENGTH_SHORT).show();
+                }
+                else if(TextUtils.isEmpty(name1)){
+                    Toast.makeText(Support.this, "Please enter your name", Toast.LENGTH_SHORT).show();
+                }
+                else if(TextUtils.isEmpty(phone1)){
+                    Toast.makeText(Support.this, "Please enter your number", Toast.LENGTH_SHORT).show();
+                }
+                else if(TextUtils.isEmpty(message1)){
+                    Toast.makeText(Support.this, "Please enter your message", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    //continue with login
+                    //TODO: add backend handling and comment
+
+                    Toast.makeText(Support.this, "Message sent", Toast.LENGTH_SHORT).show();
+                    //Intent intent= new Intent(Support.this, MainActivity.class);
+                    //startActivity(intent);
+                }
+            }
+        });
+
+
+
 
     }
 
--- app/src/main/java/com/acme/propdub/ThankYou.java
+++ app/src/main/java/com/acme/propdub/ThankYou.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ThankYou extends AppCompatActivity {
 
@@ -11,6 +13,15 @@ public class ThankYou extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thank_you);
+
+        ImageView close;
+        close= findViewById(R.id.close);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 
     @Override
