d4b76bf6025f722b762d49e64b3f396bdb7dbb18
Akero <rish1994@gmail.com>
2023-06-12 15:43:09 +0530

    schedule visit

--- app/src/main/java/com/acme/propdub/ScheduleVisit.java
+++ app/src/main/java/com/acme/propdub/ScheduleVisit.java
@@ -6,10 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class ScheduleVisit extends AppCompatActivity {
 
@@ -23,10 +28,12 @@ public class ScheduleVisit extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.drawable.back_button);
             actionBar.setTitle("Schedule a Visit");}
+
         Spinner spinner;
         Spinner spinner1;
         Spinner spinner2;
 
+//TODO: implement the 3 spinners
         spinner = findViewById(R.id.spinner);
         spinner1 = findViewById(R.id.spinner1);
         spinner2 = findViewById(R.id.spinner2);
@@ -38,7 +45,49 @@ public class ScheduleVisit extends AppCompatActivity {
 
         spinner.setAdapter(adapter);
 
+        EditText name, phone, email, message;
+        name= findViewById(R.id.editText0);
+        phone= findViewById(R.id.editText1);
+        email= findViewById(R.id.editText2);
+        message= findViewById(R.id.edittext3);
 
+
+        Button btn;
+        btn= findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                String name1, phone1, email1, message1;
+                name1= name.getText().toString();
+                phone1= phone.getText().toString();
+                email1= email.getText().toString();
+                message1= message.getText().toString();
+
+                if(TextUtils.isEmpty(email1)){
+                    Toast.makeText(ScheduleVisit.this, "Please enter your email", Toast.LENGTH_SHORT).show();
+                }
+                // Check if the input is a valid email
+                else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email1).matches()) {
+                    // Input is not a valid email, show a message to the user
+                    Toast.makeText(ScheduleVisit.this, "Please enter a valid email", Toast.LENGTH_SHORT).show();
+                }
+                else if(TextUtils.isEmpty(name1)){
+                    Toast.makeText(ScheduleVisit.this, "Please enter your name", Toast.LENGTH_SHORT).show();
+                }
+                else if(TextUtils.isEmpty(phone1)){
+                    Toast.makeText(ScheduleVisit.this, "Please enter your number", Toast.LENGTH_SHORT).show();
+                }
+                else if(TextUtils.isEmpty(message1)){
+                    Toast.makeText(ScheduleVisit.this, "Please enter your message", Toast.LENGTH_SHORT).show();
+                }else {
+                //TODO: change this
+                Toast.makeText(ScheduleVisit.this, "Your visit has been scheduled!", Toast.LENGTH_LONG).show();
+            }
+            }
+        }
+        );
 
         // Example: Retrieving the selected item from the spinner
         String selectedItem = spinner.getSelectedItem().toString();
@@ -60,17 +109,6 @@ public class ScheduleVisit extends AppCompatActivity {
         // Example: Retrieving the selected item from the spinner
         String selectedItem2 = spinner.getSelectedItem().toString();
 
-
-
-
-
-
-
-        //blurring background image
-      /*  ImageView imageView = findViewById(R.id.background_image6);
-        Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.bg_about_us);
-        Bitmap blurredBitmap = BlurBuilder.blur(this, originalBitmap);
-        imageView.setImageBitmap(blurredBitmap); */
     }
 
     @Override
@@ -85,5 +123,4 @@ public class ScheduleVisit extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
-
 }
